352e93064cbc55b40b6b155b31ed9d3d36558001
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-02-08 15:23:06 +0100

    extended visualization

--- rsvis/tools/rsshow.py
+++ rsvis/tools/rsshow.py
@@ -7,12 +7,44 @@
 import rsvis.__init__
 import rsvis.tools.image
 import rsvis.tools.rsshowui
+import rsvis.utils.ply
 
 import cv2
 import numpy as np
 import tifffile
 import tempfile
 from matplotlib import pyplot as plt
+import pandas
+import subprocess
+
+# def get_pointcloud(image, height):
+#     img = cv2.resize(image, (256, 256), interpolation = cv2.INTER_LINEAR).astype("uint8")
+#     height = cv2.resize(height, (256, 256), interpolation = cv2.INTER_LINEAR).astype(float)/8
+#     grid = np.indices((256, 256))
+#     img_dict = np.concatenate((
+#         grid[0,:,:].reshape((256*256,1)), 
+#         grid[1,:,:].reshape((256*256,1)), 
+#         height.reshape((256*256,1)), 
+#         img[:,:,0].reshape((256*256,1)), 
+#         img[:,:,1].reshape((256*256,1)), 
+#         img[:,:,2].reshape((256*256,1))
+#         ), axis=1)
+#     # img_dict = {
+#     #     'x':     [grid[0,:,:].reshape((256*256,1)).astype(float)],
+#     #     'y':     [grid[1,:,:].reshape((256*256,1)).astype(float)],
+#     #     'z':     [height.reshape((256*256,1)).astype(float)], 
+#     #     'red':   [img[:,:,0].reshape((256*256,1))], 
+#     #     'green': [img[:,:,1].reshape((256*256,1))], 
+#     #     'blue':  [img[:,:,2].reshape((256*256,1))]
+#     # }
+#     data = pandas.DataFrame(img_dict)
+#     tmp = tempfile.mkstemp(suffix=".ply")
+#     # data  = pandas.DataFrame(img_list, columns=['x', 'y', 'z', 'red', 'green', 'blue'])
+
+#     rsvis.__init__._logger.debug("Write {0}".format(tmp[1]))
+#     rsvis.utils.ply.write_ply(tmp[1], points=data, mesh=None, as_text=True)
+#     subprocess.Popen(['C:\\Program Files\\ccViewer\\ccViewer.exe', tmp[1]])
+#     return image
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
@@ -33,24 +65,63 @@ def get_image(image_path: str, image_type: str):
 
     return image
 
-def get_histogram(img):
+def get_img_info(img):
+    if len(img.shape) != 3: 
+        img_new = np.ndarray((*img.shape,1), dtype=img.dtype)
+        img_new[:,:,0] = img
+        img = img_new
+    return (img, img.shape[0], img.shape[1], img.shape[2], img.shape[0] * img.shape[1])
 
-    hist,bins = np.histogram(img.flatten(),256,[0,256])
+def get_histogram(img, **kwargs):
+    img = get_masked_image(img, **kwargs) 
 
-    cdf = hist.cumsum()
-    cdf_normalized = cdf * hist.max()/ cdf.max()
-
-    fig = plt.figure()
+    fig = plt.figure(dpi=256, facecolor='w', edgecolor='k')
     ax = fig.add_subplot(111)
-    ax.plot(cdf_normalized, color = 'b')
-    ax.hist(img.flatten(),256,[0,256], color = 'r')
-    ax.set_xlim([0,256])
-    ax.legend(('cdf','histogram'), loc = 'upper left')
 
+    img, _ , _ , img_channel, img_pixel = get_img_info(img)
+    for i in range(img_channel):
+        histr = cv2.calcHist([img],[i],None,[256],[0,256]) / img_pixel
+        plt.plot(histr)
+        
+    ax.set_xlim([0,256])
+    # ax.legend(loc = 'upper left')
+    return get_nparray_from_fig(fig)
+
+def get_nparray_from_fig(fig):  
     fig.canvas.draw()
     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return data
+
+def get_stats(img, **kwargs):
+    img = get_masked_image(img, **kwargs)
+
+    fig = plt.figure(dpi=256, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(111)
+
+    img, _ , _ , img_channel, img_pixel = get_img_info(img)
+    for i in range(img_channel):
+        vector = img[:,:,i].flatten()
+        hist, _ = np.histogram(vector , 256, [0,256])
+
+        cdf = hist.cumsum()
+        cdf_normalized = cdf * hist.max()/ cdf.max() / img_pixel
+        
+        histr = cv2.calcHist([img],[i],None,[256],[0,256]) / img_pixel
+        plt.plot(histr)
+        plt.plot(cdf_normalized, color = 'b')
+    
+    ax.set_xlim([0,256])
+    # ax.legend(loc = 'upper left')
+    return get_nparray_from_fig(fig)
+
+def get_masked_image(image, ref_point=None):
+    if ref_point:
+        return image[
+            ref_point[0][0]:ref_point[1][0],
+            ref_point[0][1]:ref_point[1][1]
+        ]
+    return image
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
@@ -62,7 +133,16 @@ def convert_image_to_color(image):
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
-def normalization(image):
+def convert_image_to_gray(image):
+    if len(image.shape)==3:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    return image
+
+#   function ----------------------------------------------------------------
+# ---------------------------------------------------------------------------
+def normalization(image, **kwargs):
+    image = convert_image_to_gray(image)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     image = clahe.apply(image)
     return image
@@ -72,9 +152,8 @@ def normalization(image):
 # ---------------------------------------------------------------------------
 def normalize_image(image):
     image = image.astype(float)
-    min_img = np.min(image)
-    max_img = np.max(image)
-    image = (image - min_img)/(max_img - min_img) * 256
+    min_max_img = (np.min(image), np.max(image))
+    image = (image - min_max_img[0])/(min_max_img[1] - min_max_img[0]) * 256
     image = image.astype("uint8")
     return image
     
@@ -102,32 +181,39 @@ def correct_image(image):
 def label_to_image(image):
     return image
 
-
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
 def rsshow(files, types):
 
     load = lambda index: [rsvis.tools.rsshow.get_image(f, t) for f, t in zip(files[index], types)]
     ui = rsvis.tools.rsshowui.RSShowUI(len(files), load) 
-
+    
+    types.index("height")
     ui.set_keys( 
         {
-            "h": lambda image: rsvis.tools.rsshow.get_histogram(image),
-            "n": lambda image: rsvis.tools.rsshow.normalization(image)
+            "h": { "func" : lambda image, **kwargs: rsvis.tools.rsshow.get_histogram(image, **kwargs), "param" : [-1]},
+            "j": { "func" : lambda image, **kwargs: rsvis.tools.rsshow.get_stats(image, **kwargs), "param" : [-1]},
+            "n": { "func" : lambda image, **kwargs: rsvis.tools.rsshow.normalization(image, **kwargs), "param" : [-1]},
+            # "r": { "func" : lambda image, height: rsvis.tools.rsshow.get_pointcloud(image, height), "param" : [-1,types.index("height")]}
         }
     )
 
 
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow('image', ui.start())
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    cv2.imshow("image", ui.start())
+    cv2.setMouseCallback("image", ui.mouse_event)
 
+    image = ui.show()
     while True:
         key = chr(cv2.waitKeyEx(1) & 0xFF)
         if ui.has_key(key):
-            image = ui.event(key)
-            if image is not None: 
-                cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-                cv2.imshow('image',image)
-            else:
+            event_result = ui.key_event(key)
+            if isinstance(event_result, int) == 1: 
                 return 1
+            elif isinstance(event_result, np.ndarray):
+                image = event_result
+        
+            cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+            cv2.imshow("image",image)
+
     cv2.destroyAllWindows()    
--- rsvis/tools/rsshowui.py
+++ rsvis/tools/rsshowui.py
@@ -2,6 +2,8 @@
 #   rsshowui.py -------------------------------------------------------------
 # ===========================================================================
 
+import cv2
+
 #   class -------------------------------------------------------------------
 # ---------------------------------------------------------------------------
 class RSShowUI():
@@ -17,6 +19,8 @@ class RSShowUI():
         self._image_index = 0
         self._file_index = 0
         self.load()
+
+        self._ref_point = []
         
         self.set_keys(keys)
 
@@ -33,16 +37,40 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def event(self, argument):
+    def key_event(self, argument):
         # Get the method from 'self'. Default to a lambda.
         if hasattr(self, self.get_method_name(argument)):
             method = getattr(self, self.get_method_name(argument), lambda: "Invalid key")
             # Call the method as we return it
             return method()
         elif argument in self._keys:
-            return self._keys[argument](self.show())
-    
-        # return
+
+
+            param = [self.show(index=p) for p in self._keys[argument]["param"]]
+            # if isinstance(self._keys[argument]["param"], list):
+            #     param = [self.show(index=p) for p in self._keys[argument]["param"]]
+            #     return self._keys[argument]["func"](*param)
+            # else:
+            return self._keys[argument]["func"](
+                *param, 
+                ref_point =self._ref_point 
+            )
+
+    #   method --------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    def mouse_event(self, event, x, y, flags, param):
+        
+        # If the left mouse button was clicked, record the starting
+	    # (x, y) coordinates and indicate that cropping is being
+	    # performed
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self._ref_point = [(x, y)]
+
+        # check to see if the left mouse button was released
+        elif event == cv2.EVENT_LBUTTONUP:
+            # record the ending (x, y) coordinates and indicate that
+            # the cropping operation is finished
+            self._ref_point.append((x, y))
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
@@ -51,8 +79,10 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def show(self):
-        return self._images[self._image_index]
+    def show(self, index=None):
+        if not index or index == -1:
+            return self._images[self._image_index]
+        return self._images[index]
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
@@ -66,12 +96,22 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_e(self):
+    def key_e(self, **kwargs):
         return self.show()
+
+#   method --------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    def key_p(self, **kwargs):
+        print("Image: '{0}', Channel: '{1}'\nRegion of Interest: '{2}'".format(self._files, self._image_index, self._ref_point))
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_s(self):
+    def key_c(self, **kwargs):
+        self._ref_point = []
+
+    #   method --------------------------------------------------------------
+    # -----------------------------------------------------------------------
+    def key_s(self, **kwargs):
         self._image_index -= 1
         if self._image_index == -1:
             self._image_index = len(self._images)-1
@@ -79,7 +119,7 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_w(self):
+    def key_w(self, **kwargs):
         self._image_index += 1
         if self._image_index == len(self._images):
             self._image_index = 0
@@ -87,7 +127,7 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_a(self):
+    def key_a(self, **kwargs):
         self._file_index -= 1
         if self._file_index == -1:
             self._file_index = self._files-1
@@ -96,7 +136,7 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_d(self):
+    def key_d(self, **kwargs):
         self._file_index += 1
         if self._file_index == self._files:
             self._file_index = 0
@@ -105,5 +145,5 @@ class RSShowUI():
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
-    def key_q(self):
-        return None
+    def key_q(self, **kwargs):
+        return 1
