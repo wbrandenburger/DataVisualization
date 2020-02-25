762e7612cd710e852a1b995f6cef21fd2adb8381
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-02-26 00:18:13 +0100

    show channels

--- rsvis/tools/rsshowui.py
+++ rsvis/tools/rsshowui.py
@@ -111,7 +111,7 @@ class RSShowUI():
         if path:
             return img_container.path
 
-        if self._index_channel is not None:
+        if not isinstance(self._index_channel, "rsvis.tools.index.Index"):
             print("Huhu")
             img = img_container.get_data(self._index_channel())
             self._index_channel.next()
