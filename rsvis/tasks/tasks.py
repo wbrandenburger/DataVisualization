7171d21eb691aed37fd35c80939dbde5ecf26a7a
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-02-29 15:53:52 +0100

    integration of test data

--- rsvis/tasks/tasks.py
+++ rsvis/tasks/tasks.py
@@ -20,10 +20,10 @@ get_value = lambda obj, key, default: obj[key] if key in obj.keys() else default
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
-def task_rsshow():
+def task_rsshow(setting):
     try:
         rsvis.tools.rsshow.rsshow(
-            rsvis.config.settings._DATA, 
+            rsvis.config.settings.get_data(setting),
             rsvis.config.settings._SETTINGS["data-tensor-types"],
             **rsvis.config.settings._SETTINGS["output"],
             param_label=get_value(rsvis.config.settings._SETTINGS,"param_label", dict()),
@@ -32,7 +32,17 @@ def task_rsshow():
             param_dist=get_value(rsvis.config.settings._SETTINGS,"param_dist", dict())
         )
     except KeyError:
-        pass
+        pass    
+
+#   function ----------------------------------------------------------------
+# ---------------------------------------------------------------------------
+def task_rsshow_training():
+    task_rsshow("training")
+
+#   function ----------------------------------------------------------------
+# ---------------------------------------------------------------------------
+def task_rsshow_test():
+    task_rsshow("test")
 
 #   function ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
@@ -50,4 +60,4 @@ def task_print_user_data():
     
     # print user's defined data
     rsvis.__init__._logger.info("Print user's defined data")
-    rsvis.utils.format.print_data(rsvis.config.settings._DATA)
+    rsvis.utils.format.print_data(rsvis.config.settings.get_data_dict())
