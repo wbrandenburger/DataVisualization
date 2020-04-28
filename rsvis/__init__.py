115a27f4d1dbc2f002806dc2dd01e82fc6b6a588
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-04-28 14:44:21 +0200

    Changed installation requirements.

--- rsvis/__init__.py
+++ rsvis/__init__.py
@@ -10,7 +10,7 @@ import os
 #   settings ----------------------------------------------------------------
 # ---------------------------------------------------------------------------
 __license__ = "MIT"
-__version__ = '0.1'
+__version__ = '0.2.3'
 __author__ = __maintainer__ = "Wolfgang Brandenburger"
 __email__ = "wolfgang.brandenburger@outlook.com"
 
@@ -20,24 +20,39 @@ try:
     import colorama
     colorama.init()
 
-    log_format = (
-        'File "%(pathname)s", line %(lineno)s:\n' +
-        colorama.Fore.YELLOW +
-        '%(levelname)s' +
-        ':' +
-        colorama.Fore.GREEN +
-        '%(name)s' +
-        colorama.Fore.CYAN +
-        ':' +
-        '%(message)s' +
-        colorama.Style.RESET_ALL
-    )
-
+    if os.environ.get("RSVIS_DEBUG"):
+        log_format = (
+            'File "%(pathname)s", line %(lineno)s:\n' +
+            colorama.Fore.YELLOW +
+            '%(levelname)s' +
+            ':' +
+            colorama.Fore.GREEN +
+            '%(name)s' +
+            colorama.Fore.CYAN +
+            ':' +
+            '%(message)s' +
+            colorama.Style.RESET_ALL
+        )
+    else:        
+        log_format = (
+            colorama.Fore.YELLOW +
+            '%(levelname)s' +
+            ':' +
+            colorama.Fore.GREEN +
+            '%(name)s' +
+            colorama.Fore.CYAN +
+            ':' +
+            '%(message)s' +
+            colorama.Style.RESET_ALL
+        )
+          
     logging.basicConfig(format=log_format)
-
     _logger = logging.getLogger("rsvis")
-    _logger.setLevel(logging.DEBUG)
+
     if os.environ.get("RSVIS_DEBUG"):
         _logger.setLevel(logging.DEBUG)
+    else:
+        _logger.setLevel(logging.INFO)
+
 except ImportError:
     pass
