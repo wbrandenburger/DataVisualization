1f3d415f5f31e94111b45317a76a8258d962f12d
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-05-05 10:54:30 +0200

    Toplevel window renamed.

--- rsvis/rsshow/rsshowui.py
+++ rsvis/rsshow/rsshowui.py
@@ -95,41 +95,41 @@ class RSShowUI():
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
     def imshow(self, wait=False):
-        self.window.mainloop()
+        self._root.mainloop()
 
     def initialize_window(self):
-        self.window = Tk()
-        self.window.title(
+        self._root = Tk()
+        self._root.title(
             "RSVis - Exploring and Viewing RS-Data"
         )
-        self.window.geometry("1000x700")
+        self._root.geometry("1000x700")
         # https://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
-        self.menubar = Menu(self.window)
+        self.menubar = Menu(self._root)
         filemenu = Menu(self.menubar, tearoff=0)
         filemenu.add_command(label="Open")
         # Save the currently displayed image to a given folder.
         filemenu.add_command(label="Save", command=lambda obj=self: obj._img_out(obj.get_img(path=True), obj.get_window_img(), prefix=obj.get_label()))
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=self.window.quit)
+        filemenu.add_command(label="Exit", command=self._root.quit)
         self.menubar.add_cascade(label="File", menu=filemenu)
         
         self.add_option_menu(self._options, label="Options")
-        self.window.config(menu=self.menubar)
+        self._root.config(menu=self.menubar)
         
         infomenu = Menu(self.menubar, tearoff=0)
         infomenu.add_command(label="Help", command=self.show_help)
         self.menubar.add_cascade(label="Information", menu=infomenu)
 
-        self.window.columnconfigure(0)
-        self.window.columnconfigure(1)
-        self.window.columnconfigure(2, weight=1)
-        self.window.rowconfigure(0, pad=3, weight=1)
-        self.window.rowconfigure(1, pad=3)
-        self.window.rowconfigure(2, pad=3)
-        self.window.rowconfigure(3, pad=3)
+        self._root.columnconfigure(0)
+        self._root.columnconfigure(1)
+        self._root.columnconfigure(2, weight=1)
+        self._root.rowconfigure(0, pad=3, weight=1)
+        self._root.rowconfigure(1, pad=3)
+        self._root.rowconfigure(2, pad=3)
+        self._root.rowconfigure(3, pad=3)
 
-        self.scrollbar = Scrollbar(self.window, orient="vertical", width=16)
-        self.listbox = Listbox(self.window, yscrollcommand=self.scrollbar.set, width=37)
+        self.scrollbar = Scrollbar(self._root, orient="vertical", width=16)
+        self.listbox = Listbox(self._root, yscrollcommand=self.scrollbar.set, width=37)
         self.scrollbar.config(command=self.listbox.yview)
         self.scrollbar.grid(row=0, column=0, sticky=N+S)
         self.listbox.grid(row=0, column=1, sticky=N+S)
@@ -138,21 +138,21 @@ class RSShowUI():
            self.listbox.insert(END, pathlib.Path(item[0].path).stem)
         self.set_img_from_index()
 
-        self.cbox_area = rsvis.tools.settingsbox.ComboBox(self.window, "Histogram", ["Grid", "Objects"], self.set_area_event)
+        self.cbox_area = rsvis.tools.settingsbox.ComboBox(self._root, "Histogram", ["Grid", "Objects"], self.set_area_event)
         self.cbox_area.grid(row=1, column=0, columnspan=2, sticky=N+S)
 
-        self.cbox_class = rsvis.tools.settingsbox.ComboBox(self.window, "Class", [c["name"] for c in self._classes], self.set_class )
+        self.cbox_class = rsvis.tools.settingsbox.ComboBox(self._root, "Class", [c["name"] for c in self._classes], self.set_class )
         self.cbox_class.grid(row=2, column=0, columnspan=2, sticky=N+S)
 
-        self.grid_settingsbox = rsvis.tools.settingsbox.SettingsBox(self.window,  ["Dimension x (Grid)", "Dimension y (Grid)"],  self.set_grid, default=self._grid)
+        self.grid_settingsbox = rsvis.tools.settingsbox.SettingsBox(self._root,  ["Dimension x (Grid)", "Dimension y (Grid)"],  self.set_grid, default=self._grid)
         self.grid_settingsbox.grid(row=3, column=0, columnspan=2, sticky=N+S)
         self.grid_settingsbox.button_set.grid(row=4, column=0, columnspan=2,)
     
-        self.canvas = rsvis.tools.canvas_image.ImageCanvas(self.window, bg="black", grid=self._grid, double_button=self.new_popup, classes=self._classes, logger=self._logger)
+        self.canvas = rsvis.tools.canvas_image.ImageCanvas(self._root, bg="black", grid=self._grid, double_button=self.new_popup, classes=self._classes, logger=self._logger)
         self.set_img(self.img, show=True)
         self.canvas.grid(row=0, column=2, rowspan=5, sticky=N+S+E+W)
         
-        self.window.bind("<F1>", self.show_help)
+        self._root.bind("<F1>", self.show_help)
         self.canvas.bind("<Key>", self.key_event)
         
     #   method --------------------------------------------------------------
@@ -196,7 +196,7 @@ class RSShowUI():
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
     def new_popup(self, title="Box", dtype="msg", value=""):
-        t = rsvis.tools.topwindow.TopWindow(self.window, title=title, dtype=dtype, value=value, command=self.quit_window)
+        t = rsvis.tools.topwindow.TopWindow(self._root, title=title, dtype=dtype, value=value, command=self.quit_window)
         t.wm_deiconify()
         t.mainloop()
 
@@ -419,7 +419,7 @@ class RSShowUI():
     # -----------------------------------------------------------------------
     def key_q(self, **kwargs):
         """Exit RSVis."""
-        self.window.destroy()
+        self._root.destroy()
 
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
