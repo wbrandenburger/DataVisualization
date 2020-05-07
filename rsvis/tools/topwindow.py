0f7fa3146908561f17e32df7d2e78f2d8e6d991d
Wolfgang Brandenburger <wolfgang.brandenburger@live.de>
2020-05-07 23:16:02 +0200

    Fixed several bugs.

--- rsvis/tools/topwindow.py
+++ rsvis/tools/topwindow.py
@@ -28,7 +28,7 @@ class TopWindow(Toplevel):
         Toplevel.__init__(self, parent)
         self.wm_title(title)
 
-        self.columnconfigure(0, weight=0)
+        self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1)
