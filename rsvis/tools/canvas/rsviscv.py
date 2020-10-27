281c52f2e21652e5198ace30e4b6445229ccb27c
Wolfgang Brandenburger <w.brandenburger@unibw.de>
2020-10-27 16:40:47 +0100

    visualize bbox

--- rsvis/tools/canvas/rsviscv.py
+++ rsvis/tools/canvas/rsviscv.py
@@ -149,6 +149,7 @@ class RSVisCanvas(extimgconcv.ExtendedImgConCv):
     #   method --------------------------------------------------------------
     # -----------------------------------------------------------------------
     def get_object_colors(self):
+        self._boxes
         return [self._color[b["label"]] for b in self._boxes if b]
 
     #   method --------------------------------------------------------------
